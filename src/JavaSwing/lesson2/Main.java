16e895646f4922b29de0e2b84a2cc247456355b0
Bahodur <tursunovb18@gmail.com>
2023-03-20 23:25:26 +0300

    вывели сообщение hello world в окне

--- src/JavaSwing/lesson2/Main.java
+++ src/JavaSwing/lesson2/Main.java
@@ -7,16 +7,27 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        Font font = new Font("", Font.BOLD, 20);
+        JFrame frame = getFrame();
+        frame.add(new MyComponent());
 //        String[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames(); // эта команда предназначена для показа всех доступных стилей в винде
 //        for (String s : fonts) {
 //            System.out.println(s);
 //        }
     }
 
+    static class MyComponent extends JComponent { // при рисовании мы переопределяем метод paintComponent
+        @Override
+        protected void paintComponent(Graphics g) {
+            Font font = new Font("Yu Gothic Medium", Font.BOLD, 20);
+            Graphics2D g2 = (Graphics2D)g; // вот этот метод
+            g2.setFont(font);
+            g2.drawString("hello world!", 20, 20);
+        }
+    }
+
     static JFrame getFrame() {
-        JFrame frame = new JFrame();
+        JFrame frame = new JFrame(){};
+        frame.setTitle("Just message!");
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Toolkit toolkit = Toolkit.getDefaultToolkit();
